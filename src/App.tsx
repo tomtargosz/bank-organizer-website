1c1d4cd96ae308ee0e8b6f3d2eb01d40e7b97493
Tom Targosz <tom.targosz@yahoo.com>
2023-01-08 17:47:12 -0600

    add React files

---
+++ src/App.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import './App.css';
+
+type BankItem = {
+  position: number;
+  id: number;
+  name: string;
+};
+
+function App(): React.ReactElement {
+  const [bankContents, setBankContents] = React.useState<BankItem[]>([
+    {
+      position: 0,
+      id: 4151,
+      name: 'Abyssal whip'
+    },
+    {
+      position: 1,
+      id: 1163,
+      name: 'Rune full helm'
+    },
+    {
+      position: 6,
+      id: 10372,
+      name: 'Zamorak chaps'
+    },
+    {
+      position: 12,
+      id: 10374,
+      name: 'Zamorak coif'
+    }
+  ]);
+  const [draggedItem, setDraggedItem] = React.useState<BankItem | null>(null);
+  const [activeSpot, setActiveSpot] = React.useState<number | null>(null);
+
+  const setActiveSpotWhenNoDraggedItem = (i: number | null): void => {
+    draggedItem != null && setActiveSpot(i);
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(8, 36px)',
+          gridTemplateRows: 'repeat(100, 32px)'
+        }}>
+        {Array.from(Array(800)).map((_, i) => {
+          const item = bankContents.find((content) => content.position === i);
+          const isActive = activeSpot === i;
+
+          if (item !== undefined) {
+            return (
+              <div key={i} className="bankSlot">
+                <img
+                  draggable
+                  onDragStart={() => setDraggedItem(item)}
+                  onDrop={(e) => {
+                    if (draggedItem == null) {
+                      console.error('No dragged item in onDrop');
+                      return;
+                    }
+
+                    setBankContents(
+                      bankContents
+                        .filter((i) => i.id !== draggedItem?.id && i.id !== item.id)
+                        .concat([
+                          { ...draggedItem, position: i },
+                          { ...item, position: draggedItem.position }
+                        ])
+                    );
+                    setActiveSpotWhenNoDraggedItem(i);
+                    setDraggedItem(null);
+                  }}
+                  onMouseOver={() => setActiveSpotWhenNoDraggedItem(i)}
+                  onMouseOut={() => setActiveSpotWhenNoDraggedItem(i)}
+                  onDragOver={(e) => {
+                    e.preventDefault();
+                    setActiveSpotWhenNoDraggedItem(i);
+                  }}
+                  style={{
+                    outline: draggedItem != null && isActive ? '1px solid red' : ''
+                  }}
+                  key={i}
+                  src={`https://raw.githubusercontent.com/osrsbox/osrsbox-db/master/docs/items-icons/${item.id}.png`}
+                />
+              </div>
+            );
+          } else {
+            return (
+              <div
+                key={i}
+                onDragOver={(e) => {
+                  e.preventDefault();
+                  setActiveSpotWhenNoDraggedItem(i);
+                }}
+                style={{
+                  outline: draggedItem != null && isActive ? '1px solid red' : ''
+                }}
+                onDrop={(e) => {
+                  if (draggedItem == null) {
+                    console.error('No dragged item in onDrop');
+                    return;
+                  }
+                  setBankContents(
+                    bankContents
+                      .filter((i) => i.id !== draggedItem.id)
+                      .concat([{ ...draggedItem, position: i }])
+                  );
+                  setActiveSpotWhenNoDraggedItem(i);
+                  setDraggedItem(null);
+                }}
+                onMouseOver={() => {
+                  setActiveSpotWhenNoDraggedItem(i);
+                }}
+                onMouseOut={() => {
+                  setActiveSpotWhenNoDraggedItem(i);
+                }}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
